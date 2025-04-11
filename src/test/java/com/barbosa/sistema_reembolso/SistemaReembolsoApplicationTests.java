600841013dea836731d3e82c3f9a21e21562ff84
talisson-b99 <talissonb422@gmail.com>
2025-04-11 18:26:42 -0300

    chore: configurar estrutura inicial do projeto (closes #2)

---
+++ src/test/java/com/barbosa/sistema_reembolso/SistemaReembolsoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.barbosa.sistema_reembolso;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemaReembolsoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
