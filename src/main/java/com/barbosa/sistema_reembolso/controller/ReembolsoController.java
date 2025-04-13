5d29c05a5174104039c8e4c8edd165b47eeab582
talisson-b99 <talissonb422@gmail.com>
2025-04-12 23:15:04 -0300

    feat: implementar listagem e consulta de reembolsos (closes #5)

--- src/main/java/com/barbosa/sistema_reembolso/controller/ReembolsoController.java
+++ src/main/java/com/barbosa/sistema_reembolso/controller/ReembolsoController.java
@@ -1,17 +1,17 @@
 package com.barbosa.sistema_reembolso.controller;
 
+import com.barbosa.sistema_reembolso.domain.enums.StatusReembolso;
 import com.barbosa.sistema_reembolso.domain.model.Reembolso;
 import com.barbosa.sistema_reembolso.dto.ReembolsoRequestDTO;
 import com.barbosa.sistema_reembolso.dto.ReembolsoResponseDTO;
 import com.barbosa.sistema_reembolso.service.ReembolsoService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.net.URI;
+import java.util.List;
+import java.util.UUID;
 
 @RestController
 @RequestMapping("/reembolsos")
@@ -25,6 +25,21 @@ public class ReembolsoController {
         var reembolso = reembolsoService.cadastrarReembolso(dto);
 
         return ResponseEntity.created(URI.create("/reembolsos/" + reembolso.id())).build();
+    }
+
+    @GetMapping
+    public ResponseEntity<List<ReembolsoResponseDTO>> buscarTodosReembolsos(){
+        return ResponseEntity.ok(reembolsoService.buscarTodosReembolsos());
+    }
+
+    @GetMapping("/{reembolsoId}")
+    public ResponseEntity<ReembolsoResponseDTO> buscarReembolsoPorId(@PathVariable UUID reembolsoId){
+        return ResponseEntity.ok(reembolsoService.buscarReembolsoPorId(reembolsoId));
+    }
+
+    @GetMapping("/search")
+    public ResponseEntity<List<ReembolsoResponseDTO>> buscarReembolsoPorStatus(@RequestParam("status")StatusReembolso status){
+        return ResponseEntity.ok(reembolsoService.buscarReembolsoPorStatus(status));
     }
 
 }
--- src/main/java/com/barbosa/sistema_reembolso/repository/ReembolsoRepository.java
+++ src/main/java/com/barbosa/sistema_reembolso/repository/ReembolsoRepository.java
@@ -1,9 +1,13 @@
 package com.barbosa.sistema_reembolso.repository;
 
+import com.barbosa.sistema_reembolso.domain.enums.Role;
+import com.barbosa.sistema_reembolso.domain.enums.StatusReembolso;
 import com.barbosa.sistema_reembolso.domain.model.Reembolso;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.UUID;
 
 public interface ReembolsoRepository extends JpaRepository<Reembolso, UUID> {
+    List<Reembolso>findAllByStatus(StatusReembolso status);
 }
